634c6cc2c26a64f566ac7ab490c60ca2777c50a4
Daniel Tomlinson <Daniel Tomlinson@DESKTOP-67G87PL>
2021-04-09 17:21:13 +1200

    next push

--- working covid simulation/Program.cs
+++ working covid simulation/Program.cs
@@ -5,6 +5,7 @@ namespace covid_simulation_game
 {
     class Program
     {
+        // hi
         // comment
         // creating my variables
         static double currentCases = 0;
@@ -32,6 +33,9 @@ namespace covid_simulation_game
         static string iccalationFacility = "tbc";
         static double recovered = 0;
         static string[] vaccinations = new string[4];
+        static bool areYouVaccinated = false;
+        static string vaccinationsChoice = "tbc";
+        static double vaccinationSpreadRate = 0;
         static void Main(string[] args)
         {
             //getting my colours and telling the user what is happening
@@ -125,113 +129,142 @@ namespace covid_simulation_game
                     enter = Console.ReadLine();
                     Console.Clear();
                 }
+                
 
                 if (weekNum > 5 && weekNum <= 2000)
                 {
 
                     Console.WriteLine("do you want to start or stay in lockdown (yes or no)" + "\n" + " this will decrease  your bank value by 400 million per week but remember to save enough  money for your vaccinations");
                     Console.WriteLine(" vaccinations  cost 1 billion dollars to vaccinate the whole country" + "\n" + "and a isolation facility will cost 500 million to build");
-                    lockDown = Console.ReadLine();
+                    trueOrFalse = false;
+                    while (trueOrFalse == false)
+                    {
+                        try
+                        {
+                            lockDown = Console.ReadLine();
+                            if (lockDown == "yes" || lockDown == "no")
+                                trueOrFalse = true;
+                            else
+                            {
+                                Console.WriteLine("please check your spelling, yes or no");
+                            }
+
+                        }
+                        catch
+                        {
+
+                            Console.WriteLine("please check your spelling yes or no");
+                        }
+
+                    }
                     if (weekNum > 9)
                     {
                         if (iccalationFacility != "yes")
                         {
                             Console.WriteLine("do you want to build a isolation  facility");
-                            iccalationFacility = Console.ReadLine();
-                        }
-                        if (iccalationFacility == "yes" && lockDown == "yes")
-                        {
-                            goingIntoLockdownWithAIscalationFacilityWorking();
-                            money = money - 450000000;
-                            Console.WriteLine("your total money is now $" + money);
-                            Console.WriteLine("press enter when finished");
-                            enter = Console.ReadLine();
-                            Console.Clear();
-                        }
-                        else if (iccalationFacility == "yes" && lockDown == "no")
-                        {
-                            noUserChangesWithAIscalationFacilityWorking();
-                            money = money + 50000000;
-                            Console.WriteLine("press enter when finished");
-                            enter = Console.ReadLine();
-                            Console.Clear();
-                        }
-                        else if (iccalationFacility == "no" && lockDown == "no")
-                        {
-                            noUserChanges();
-                            money = money + 50000000;
-                            Console.WriteLine("press enter when finished");
-                            enter = Console.ReadLine();
-                            Console.Clear();
-                        }
-                        else if (iccalationFacility == "no" && lockDown == "yes")
-                        {
-                            goingIntoLockDown();
-                            money = money - 450000000;
-                            Console.WriteLine("your total money is now $" + money);
-                            Console.WriteLine("press enter when finished");
-                            enter = Console.ReadLine();
-                            Console.Clear();
-                        }
-                        else
-                        {
-                            Console.WriteLine("please check your spelling");
-                            Console.WriteLine("please try again with the answer yes or no next time round");
-                            weekNum = weekNum - 2;
-                            Thread.Sleep(1000);
-                            Console.Clear();
-                        }
-                    }
-                    else if (weekNum < 9)
-                    {
-                        if (lockDown == "yes")
-                        {
-                            goingIntoLockDown();
-                            money = money - 400000000;
-                            Console.WriteLine("your total money is now $" + money);
-                            Console.WriteLine("press enter when finished");
-                            enter = Console.ReadLine();
-                            Console.Clear();
-                        }
-                        else if (lockDown == "no")
-                        {
-                            noUserChanges();
-                            money = money + 50000000;
-                            Console.WriteLine("press enter when finished");
-                            enter = Console.ReadLine();
-                            Console.Clear();
-                        }
-                        else
-                        {
-                            Console.WriteLine("please check your spelling");
-                            Console.WriteLine("please try again with the answer yes or no next time round");
-                            weekNum = weekNum - 2;
-                            Thread.Sleep(1000);
-                            Console.Clear();
-                        }
-                    }
+                            trueOrFalse = false;
+                            while (trueOrFalse = false)
+                            {
+                                try
+                                {
+                                    iccalationFacility = Console.ReadLine();
+                                    if (iccalationFacility == "yes" || iccalationFacility == "no")
+                                        trueOrFalse = true;
+                                    else
+                                    {
+                                        Console.WriteLine("please check your spelling, yes or no");
+                                    }
 
+                                }
+                                catch
+                                {
+
+                                    Console.WriteLine("please check your spelling yes or no");
+                                }
+
+
+
+                            }
+                        }
+                            if (iccalationFacility == "yes" && lockDown == "yes")
+                            {
+                                goingIntoLockdownWithAIscalationFacilityWorking();
+                                money = money - 450000000;
+                                Console.WriteLine("your total money is now $" + money);
+                                Console.WriteLine("press enter when finished");
+                                enter = Console.ReadLine();
+                                Console.Clear();
+                            }
+                            else if (iccalationFacility == "yes" && lockDown == "no")
+                            {
+                                noUserChangesWithAIscalationFacilityWorking();
+                                money = money + 50000000;
+                                Console.WriteLine("press enter when finished");
+                                enter = Console.ReadLine();
+                                Console.Clear();
+                            }
+                            else if (iccalationFacility == "no" && lockDown == "no")
+                            {
+                                noUserChanges();
+                                money = money + 50000000;
+                                Console.WriteLine("press enter when finished");
+                                enter = Console.ReadLine();
+                                Console.Clear();
+                            }
+                            else if (iccalationFacility == "no" && lockDown == "yes")
+                            {
+                                goingIntoLockDown();
+                                money = money - 450000000;
+                                Console.WriteLine("your total money is now $" + money);
+                                Console.WriteLine("press enter when finished");
+                                enter = Console.ReadLine();
+                                Console.Clear();
+                            }
+                        }
+                        if (weekNum < 9)
+                        {
+                            if (lockDown == "yes")
+                            {
+                                goingIntoLockDown();
+                                money = money - 400000000;
+                                Console.WriteLine("your total money is now $" + money);
+                                Console.WriteLine("press enter when finished");
+                                enter = Console.ReadLine();
+                                Console.Clear();
+                            }
+                            else if (lockDown == "no")
+                            {
+                                noUserChanges();
+                                money = money + 50000000;
+                                Console.WriteLine("press enter when finished");
+                                enter = Console.ReadLine();
+                                Console.Clear();
+                            }
+                        }
+
+                        
+                    }
+                    weekNum = weekNum + 2;
 
                 }
-                weekNum = weekNum + 2;
-
-            }
-            // telling me if i lost of won or lost
-            Console.ForegroundColor = ConsoleColor.Red;
-            if (totalCases > 5000000)
-            {
-                Console.WriteLine("you have too many cases");
-                Console.WriteLine("you lost");
-            }
-            if (money < 1000000)
-            {
-                Console.WriteLine("you have run out of money");
-                Console.WriteLine("you lost");
-            }
-            if (currentCases < 1)
-            {
-                Console.WriteLine("You won, congratulations");
-            }
+                
+            
+// telling me if i lost of won or lost
+                Console.ForegroundColor = ConsoleColor.Red;
+                if (totalCases > 5000000)
+                {
+                    Console.WriteLine("you have too many cases");
+                    Console.WriteLine("you lost");
+                }
+                if (money < 1000000)
+                {
+                    Console.WriteLine("you have run out of money");
+                    Console.WriteLine("you lost");
+                }
+                if (currentCases < 1)
+                {
+                    Console.WriteLine("You won, congratulations");
+                }
         }
 
 
@@ -324,7 +357,76 @@ namespace covid_simulation_game
             figuringOutTheCases();
             return 1;
         }
+        public static double VaccinationsChoices()
+        {
+            weekNum = weekNum + 2;
+            areYouVaccinated = false;
+            if (areYouVaccinated == false)
+            {
+                
+
+                Console.WriteLine("do you want to get your country vaccinated" + '\n' + "you can choose between 3 vaccinations");
+                Console.WriteLine("Astrazeneca: this vaccination is 90% effective and will cost you $500,000,000 to vaccinate");
+                Console.WriteLine("Moderna: this vaccination is 95% effective and will cost $3,400,000,000 to vaccinate the whole country");
+                Console.WriteLine("pfizer: this vaccination is 95% effective and will cost $2,000,000,000 to vaccinate the country");
+                string[] typeOfVaccinations = { "Astrazeneca", "Moderna", "pfizer" };
+                trueOrFalse = false;
+
+                string vaccinationsChoice = "tbc";
+                while (trueOrFalse == false)
+                {
+                    try
+                    {
+                        vaccinationsChoice = Console.ReadLine();
+                        if (vaccinationsChoice == "Astrazeneca" || vaccinationsChoice == "Moderna" || vaccinationsChoice == "pfizer")
+                        {
+                            trueOrFalse = true;
+                            areYouVaccinated = true;
+                        }
+                        else if (vaccinationsChoice == "no")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("please check your spelling");
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("please check your spelling");
+                    }
+                }
+                if (vaccinationsChoice == "Astrazeneca")
+                {
+                    vaccinationSpreadRate = 0.25 * orignalSpreadRate;
+                    spreadRate = vaccinationSpreadRate;
+                    money = money - 500000000;
+                    Console.WriteLine("your total money is now $" + money);
+                }
+                else if(vaccinationsChoice == "Moderna")
+                {
+                    vaccinationSpreadRate = 0.05 * orignalSpreadRate;
+                    spreadRate = vaccinationSpreadRate;
+                    money = money - 3400000000;
+                    Console.WriteLine("your total money is now $" + money);
+                }
+                else if(vaccinationsChoice == "pfizer")
+                {
+                    vaccinationSpreadRate = 0.05 * orignalSpreadRate;
+                    spreadRate = vaccinationSpreadRate;
+                    money = money - 2000000000;
+                    Console.WriteLine("your total money is now $" + money);
+                }
+            }
+
+            return 1;
+
+        }
+
 
 
     }
 }
+////https://www.geeksforgeeks.org/c-sharp-how-to-change-foreground-color-of-text-in-console/
+//https://www.bbc.com/news/world-asia-india-55748124
